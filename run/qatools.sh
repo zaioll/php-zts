f87d9895d7eef252daed4ebfbc80564ba3434d3a
zaioll <zaioll@protonmail.com>
2020-03-26 19:17:10 -0400

    Adição composer plugin

--- run/qatools.sh
+++ run/qatools.sh
@@ -5,3 +5,5 @@ echo "installing composer..."
 curl -# -L https://getcomposer.org/installer | php &> /dev/null
 mv composer.phar /usr/local/bin/composer
 chmod +x /usr/local/bin/composer
+echo "installing composer plugin..."
+/usr/bin/php -d memory_limit=-1 /usr/local/bin/composer global require hirak/prestissimo
