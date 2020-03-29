8fe3764130e5cb871872a3e312f37780a9a50cf8
zaioll <zaioll@protonmail.com>
2020-03-29 18:54:57 -0400

    Fix package search mongodb

--- install/php-mongodb.sh
+++ install/php-mongodb.sh
@@ -5,20 +5,27 @@ if [ -z ${install_base} ]; then
 fi
 
 extension_dir=$(php-config --extension-dir)
-config_dir=$(php-config --prefix)/etc/conf.d
+config_dir=$(php-config --ini-dir)
 
 # install php-mongodb
 cd ${install_base}/local/src
-version_mongodb=$(git ls-remote --tags https://github.com/mongodb/mongo-php-driver.git | egrep -o '[0-9]*\.[0-9]*\.[0-9]*$' | tail -n 1)
+version_mongodb=$(git ls-remote --tags https://github.com/mongodb/mongo-php-driver.git | egrep -o 'v?[0-9]{1,}.[0-9]{1,}.[0-9]{1,}$' | tail -n 1)
 
 echo "install"
 figlet "php-mongodb"
 echo "version ${version_mongodb}"
 
-curl --progress-bar --max-time 60 --retry-max-time 60 --retry 5 --location https://github.com/mongodb/mongo-php-driver/releases/download/${version_mongodb}/mongodb-${version_mongodb}.tgz  | tar xzf -
+curl \
+  --progress-bar \
+  --max-time 60 \
+  --retry-max-time 60 \
+  --retry 5 \
+  --location https://github.com/mongodb/mongo-php-driver/releases/download/${version_mongodb}/mongodb-${version_mongodb}.tgz  | tar xzf -
+
 mv mongodb* mongodb
 cd mongodb
 
+# configure and compile
 phpize
 ./configure
 make -j$(nproc) > >(tee /info/compile-php-mongodb.log) 2> >(tee /info/compile-php-mongodb.err >&2)
