81b70adb0942fdc03760ccaf3209967ac436b77e
zaioll <zaioll@protonmail.com>
2022-03-04 09:25:53 -0400

    Update debian base image to buster

--- install/requirements/packages.sh
+++ install/requirements/packages.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-DEBIAN_FRONTEND=noninteractive apt-get install -y $(cat /install/requirements/*packages | tr "\n" " ")
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends $(cat /install/requirements/*packages | tr "\n" " ")
 
 ln -s /usr/include/x86_64-linux-gnu/gmp.h /usr/include/gmp.h
 ln -s /usr/lib/x86_64-linux-gnu/libldap.so /usr/lib/libldap.so
 ln -s /usr/lib/x86_64-linux-gnu/libldap.a /usr/lib/libldap.a
 ln -s /usr/lib/x86_64-linux-gnu/libwebp.so.6.0.2 /usr/lib/libwebp.so.6
 ln -s /usr/lib/x86_64-linux-gnu/libXpm.so.4.11.0 /usr/lib/libXpm.so.4
-ln -s /usr/lib/x86_64-linux-gnu/libsodium.so.18 /usr/lib/libsodium.so.18
+ln -s /usr/lib/x86_64-linux-gnu/libsodium.so.23 /usr/lib/libsodium.so.23
 
 echo "Ajustando timezone, data e hora"
-export TZ=America/Cuiaba
+export TZ=${TZ-:"America/Manaus"}
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
