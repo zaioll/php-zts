d40042d7c77254faa2bd1ddad47a744a260310f3
zaioll <zaioll@protonmail.com>
2022-03-04 09:35:41 -0400

    Improve mongodb install

--- install/packages/php-mongodb.sh
+++ install/packages/php-mongodb.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+install_with_pecl=${install_with_pecl:-"yes"}
 if [ -z ${install_base} ]; then
   exit 1
 fi
@@ -7,24 +8,31 @@ if [ -z $(type -P php) ];then
   exit 1;
 fi
 
-if [ ! -d ${install_base}/local/src/mongodb ]; then
-  echo "Mongodb não encontrado!"
-  exit 1
+echo "install MongoDB: ${install_with_pecl}"
+if [ "${install_with_pecl}" = "yes" ];then
+  echo "Install Mongodb via PECL"
+  pecl install mongodb
+  figlet "php-mongodb"
+else
+  if [ ! -d ${install_base}/local/src/mongodb ]; then
+    echo "Mongodb não encontrado!"
+    exit 1
+  fi
+
+  # install php-mongodb
+  cd ${install_base}/local/src/mongodb
+  version_mongodb=$(git ls-remote --tags https://github.com/mongodb/mongo-php-driver.git | egrep -o 'v?[0-9]{1,}.[0-9]{1,}.[0-9]{1,}$' | tail -n 1)
+
+  echo "install"
+  echo "version ${version_mongodb}"
+
+  # configure and compile
+  phpize
+  ./configure
+  make -j$(nproc) > >(tee /info/compile-php-mongodb.log) 2> >(tee /info/compile-php-mongodb.err >&2)
+
+  extension_dir=$(php-config --extension-dir)
+  cp modules/mongodb.so ${extension_dir}/mongodb.so
+
+  figlet "php-mongodb"
 fi
-
-# install php-mongodb
-cd ${install_base}/local/src/mongodb
-version_mongodb=$(git ls-remote --tags https://github.com/mongodb/mongo-php-driver.git | egrep -o 'v?[0-9]{1,}.[0-9]{1,}.[0-9]{1,}$' | tail -n 1)
-
-echo "install"
-echo "version ${version_mongodb}"
-
-# configure and compile
-phpize
-./configure
-make -j$(nproc) > >(tee /info/compile-php-mongodb.log) 2> >(tee /info/compile-php-mongodb.err >&2)
-
-extension_dir=$(php-config --extension-dir)
-cp modules/mongodb.so ${extension_dir}/mongodb.so
-
-figlet "php-mongodb"
